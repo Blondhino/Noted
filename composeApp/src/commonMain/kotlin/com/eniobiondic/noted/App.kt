788c39da35c5ce4ea31012eecdf3b24b4dac7754
eniobiondic <enio.biondic@undabot.com>
2024-12-17 13:18:24 +0100

    chore: add simple usage for shared viewmodels between mobile apps

--- composeApp/src/commonMain/kotlin/com/eniobiondic/noted/App.kt
+++ composeApp/src/commonMain/kotlin/com/eniobiondic/noted/App.kt
@@ -7,29 +7,40 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import noted.composeapp.generated.resources.Res
 import noted.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.annotation.KoinExperimentalAPI
 
+@OptIn(KoinExperimentalAPI::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
+        val viewModel: TestDiViewmodel = koinViewModel()
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!!")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
+                    Text(viewModel.getHelloWorld())
                 }
             }
         }
---
+++ composeApp/src/commonMain/kotlin/com/eniobiondic/noted/TestDiViewmodel.kt
@@ -0,0 +1,7 @@
+package com.eniobiondic.noted
+
+import androidx.lifecycle.ViewModel
+
+class TestDiViewmodel : ViewModel() {
+    fun getHelloWorld() = "Hello World from shared ViewModel"
+}
